0c813bac88e343e8772f9d7776013ec2e6b13255
evhen <evgeniy.pismenny@gmail.com>
2025-11-04 00:46:45 +0200

    player rotation

--- main.py
+++ main.py
@@ -23,7 +23,9 @@ def main():
                 return
 
         screen.fill(color="black")
-
+        
+        player.update(dt)
+        
         player.draw(screen)
 
         pygame.display.flip()
