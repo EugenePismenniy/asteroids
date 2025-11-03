eba20961ff1c3db43f8bc6f408ebdf13bbe2e66f
evhen <evgeniy.pismenny@gmail.com>
2025-11-04 00:22:57 +0200

    add clock.tick

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ def main():
 
     pygame.init()
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
@@ -18,6 +21,7 @@ def main():
 
         screen.fill(color="black")
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
